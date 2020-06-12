a154b8cc59a3ca403c08ecef29d13c4cab9230ff
renato <ernesto@amigo.com>
2020-06-11 21:58:10 -0500

    Animación de números y lettering

--- js/inicio.js
+++ js/inicio.js
@@ -3,6 +3,8 @@
 /* -------------------------------------------------------------------------- */
 $(function(){
 
+    $('.nombre-sitio').lettering();
+
     /* -------------------------------------------------------------------------- */
     /*                        TABS DE PROGRACION DEL EVENTO                       */
     /* -------------------------------------------------------------------------- */
@@ -18,5 +20,22 @@ $(function(){
         // Esto es para que no haga un brinco al dar click
         return false
     })
-    
+
+    /* -------------------------------------------------------------------------- */
+    /*                        ANIMACIONES PARA LOS NUMEROS                        */
+    /* -------------------------------------------------------------------------- */
+
+    // Animacion de numeros
+    $('.resumen-evento-anmt div:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento-anmt div:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento-anmt div:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento-anmt div:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+    // Cuenta regresiva
+    $('.cuenta-regresiva').countdown('2020/07/20 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    })
 })
