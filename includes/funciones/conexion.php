7fe9417ed908e1327e5c03d0ef3009d901b90da2
renato <renato@mail.com>
2020-10-11 23:32:05 -0500

    nueva base de datos

--- includes/funciones/conexion.php
+++ includes/funciones/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = new mysqli('localhost', 'root', '', 'uw_conference');
+    $conexion = new mysqli('localhost', 'root', 'renatito', 'uw_conference');
 
     if ($conexion->connect_errno) {
         echo "Falló la conexión a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
